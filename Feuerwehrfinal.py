0e07573805672ad78e4ac14e6ed3e5db10d10f69
Lukas <43641816+Lukas5598@users.noreply.github.com>
2020-06-15 17:34:33 +0200

    Update Feuerwehrfinal.py

--- Feuerwehrfinal.py
+++ Feuerwehrfinal.py
@@ -130,6 +130,7 @@ def on_message(client, userdata, message):
     currentDT = datetime.datetime.now() #Aktuelle Uhrzeit
     print(currentDT.strftime("%Y-%m-%d %H:%M:%S")+" Nachricht erhalten: "+str(msg))
     split = msg.split(" ")
+    global task
     i=0
     a = message.topic.split("/")
     print(a)
